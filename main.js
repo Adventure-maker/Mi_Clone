9d24208a4c4460460cb85093cd1897050b63e395
Pyami <1pyami@gmail.com>
2021-05-08 08:06:38 +0530

    added videos

--- main.js
+++ main.js
@@ -66,3 +66,48 @@ signIn.addEventListener('click', e => {
         title.innerHTML = 'Xiaomi India | Mobile | TV | Smart Home | Lifestyle - Mi India'
     }, 3000);
 })
+
+
+
+
+
+
+
+
+
+
+
+// closing and opening vide modals
+
+const video1 = document.getElementById('video1')
+const video2 = document.getElementById('video2')
+const video3= document.getElementById('video3')
+const videoModal= document.getElementById('videoModal')
+const videoCont1= document.getElementById('videoCont1')
+const videoCont2= document.getElementById('videoCont2')
+const videoCont3= document.getElementById('videoCont3')
+
+video1.addEventListener('click', () => {
+    videoModal.style.display = 'block'
+    videoCont1.style.display = 'block'
+    videoCont2.style.display = 'none'
+    videoCont3.style.display = 'none'
+})
+
+video2.addEventListener('click', () => {
+    videoModal.style.display = 'block'
+    videoCont1.style.display = 'none'
+    videoCont3.style.display = 'none'
+    videoCont2.style.display = 'block'
+})
+
+video3.addEventListener('click', () => {
+    videoModal.style.display = 'block'
+    videoCont1.style.display = 'none'
+    videoCont2.style.display = 'none'
+    videoCont3.style.display = 'block'
+})
+
+function closeModal() {
+    videoModal.style.display = 'none'
+}
